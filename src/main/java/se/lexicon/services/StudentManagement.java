2162303fe5db68f8d177096c955f225497b15f3d
MikaelEngvall <mikael@engvall.org>
2023-10-11 13:08:43 +0200

    Part 3 done

---
+++ src/main/java/se/lexicon/services/StudentManagement.java
@@ -0,0 +1,14 @@
+package se.lexicon.services;
+
+import se.lexicon.model.Student;
+
+import java.util.List;
+
+public interface StudentManagement {
+    Student createStudent(String name);
+    Student findStudent(int id);
+    List<Student> findAllStudents();
+    void editStudent(int id, String newName);
+    void deleteStudent(int id);
+    void manageStudents();
+}
