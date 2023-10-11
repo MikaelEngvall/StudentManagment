e7193e1940b8c7f0da031d9991f55d9e5581ee74
MikaelEngvall <mikael@engvall.org>
2023-10-11 12:30:08 +0200

    Created the UserInputService interface and the ScannerInputService class

---
+++ src/main/java/se/lexicon/utils/ScannerInputService.java
@@ -0,0 +1,16 @@
+package se.lexicon.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScannerInputService implements UserInputService{
+    @Override
+    public String getString() {
+        return null;
+    }
+
+    @Override
+    public int getInt() {
+        return 0;
+    }
+}
